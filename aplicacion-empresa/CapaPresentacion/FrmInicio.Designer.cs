bcd200c6d8489630af48ae95fbfede7ba01f2b63
Hernan <hernandemczuk@gmail.com>
2015-06-17 12:12:08 -0300

    Cambios realizados hasta el video N°7

---
+++ aplicacion-empresa/CapaPresentacion/FrmInicio.Designer.cs
@@ -0,0 +1,290 @@
+﻿namespace CapaPresentacion
+{
+    partial class FrmInicio
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanelGeneral = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.buttonReportes = new System.Windows.Forms.Button();
+            this.buttonEmpleados = new System.Windows.Forms.Button();
+            this.buttonTransporte = new System.Windows.Forms.Button();
+            this.buttonClientes = new System.Windows.Forms.Button();
+            this.buttonCategorias = new System.Windows.Forms.Button();
+            this.buttonProductos = new System.Windows.Forms.Button();
+            this.buttonPedidos = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.aRCHIVOToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aYUDAToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.rESTAURACIÓNToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.tableLayoutPanelGeneral.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanelGeneral
+            // 
+            this.tableLayoutPanelGeneral.ColumnCount = 1;
+            this.tableLayoutPanelGeneral.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanelGeneral.Controls.Add(this.tableLayoutPanel2, 0, 1);
+            this.tableLayoutPanelGeneral.Controls.Add(this.menuStrip1, 0, 0);
+            this.tableLayoutPanelGeneral.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanelGeneral.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanelGeneral.Name = "tableLayoutPanelGeneral";
+            this.tableLayoutPanelGeneral.RowCount = 3;
+            this.tableLayoutPanelGeneral.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanelGeneral.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanelGeneral.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanelGeneral.Size = new System.Drawing.Size(873, 405);
+            this.tableLayoutPanelGeneral.TabIndex = 0;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 8;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 90F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 90F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 90F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 90F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 90F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 90F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 90F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.Controls.Add(this.buttonReportes, 6, 0);
+            this.tableLayoutPanel2.Controls.Add(this.buttonEmpleados, 5, 0);
+            this.tableLayoutPanel2.Controls.Add(this.buttonTransporte, 4, 0);
+            this.tableLayoutPanel2.Controls.Add(this.buttonClientes, 3, 0);
+            this.tableLayoutPanel2.Controls.Add(this.buttonCategorias, 2, 0);
+            this.tableLayoutPanel2.Controls.Add(this.buttonProductos, 1, 0);
+            this.tableLayoutPanel2.Controls.Add(this.buttonPedidos, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.panel1, 7, 0);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(3, 27);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 1;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(867, 111);
+            this.tableLayoutPanel2.TabIndex = 0;
+            // 
+            // buttonReportes
+            // 
+            this.buttonReportes.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonReportes.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.buttonReportes.Font = new System.Drawing.Font("Candara", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonReportes.Image = global::CapaPresentacion.Properties.Resources.reportes;
+            this.buttonReportes.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.buttonReportes.Location = new System.Drawing.Point(543, 3);
+            this.buttonReportes.Name = "buttonReportes";
+            this.buttonReportes.Size = new System.Drawing.Size(84, 105);
+            this.buttonReportes.TabIndex = 6;
+            this.buttonReportes.Text = "REPORTES";
+            this.buttonReportes.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.buttonReportes.UseVisualStyleBackColor = false;
+            // 
+            // buttonEmpleados
+            // 
+            this.buttonEmpleados.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonEmpleados.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.buttonEmpleados.Font = new System.Drawing.Font("Candara", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonEmpleados.Image = global::CapaPresentacion.Properties.Resources.empleados;
+            this.buttonEmpleados.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.buttonEmpleados.Location = new System.Drawing.Point(453, 3);
+            this.buttonEmpleados.Name = "buttonEmpleados";
+            this.buttonEmpleados.Size = new System.Drawing.Size(84, 105);
+            this.buttonEmpleados.TabIndex = 5;
+            this.buttonEmpleados.Text = "EMPLEADOS";
+            this.buttonEmpleados.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.buttonEmpleados.UseVisualStyleBackColor = false;
+            // 
+            // buttonTransporte
+            // 
+            this.buttonTransporte.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonTransporte.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.buttonTransporte.Font = new System.Drawing.Font("Candara", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonTransporte.Image = global::CapaPresentacion.Properties.Resources.transporte;
+            this.buttonTransporte.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.buttonTransporte.Location = new System.Drawing.Point(363, 3);
+            this.buttonTransporte.Name = "buttonTransporte";
+            this.buttonTransporte.Size = new System.Drawing.Size(84, 105);
+            this.buttonTransporte.TabIndex = 4;
+            this.buttonTransporte.Text = "TRANSPORTE";
+            this.buttonTransporte.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.buttonTransporte.UseVisualStyleBackColor = false;
+            // 
+            // buttonClientes
+            // 
+            this.buttonClientes.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonClientes.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.buttonClientes.Font = new System.Drawing.Font("Candara", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonClientes.Image = global::CapaPresentacion.Properties.Resources.clientes;
+            this.buttonClientes.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.buttonClientes.Location = new System.Drawing.Point(273, 3);
+            this.buttonClientes.Name = "buttonClientes";
+            this.buttonClientes.Size = new System.Drawing.Size(84, 105);
+            this.buttonClientes.TabIndex = 3;
+            this.buttonClientes.Text = "CLIENTES";
+            this.buttonClientes.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.buttonClientes.UseVisualStyleBackColor = false;
+            // 
+            // buttonCategorias
+            // 
+            this.buttonCategorias.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonCategorias.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.buttonCategorias.Font = new System.Drawing.Font("Candara", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonCategorias.Image = global::CapaPresentacion.Properties.Resources.categorias;
+            this.buttonCategorias.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.buttonCategorias.Location = new System.Drawing.Point(183, 3);
+            this.buttonCategorias.Name = "buttonCategorias";
+            this.buttonCategorias.Size = new System.Drawing.Size(84, 105);
+            this.buttonCategorias.TabIndex = 2;
+            this.buttonCategorias.Text = "CATEGORÍAS";
+            this.buttonCategorias.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.buttonCategorias.UseVisualStyleBackColor = false;
+            // 
+            // buttonProductos
+            // 
+            this.buttonProductos.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonProductos.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.buttonProductos.Font = new System.Drawing.Font("Candara", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonProductos.Image = global::CapaPresentacion.Properties.Resources.productos;
+            this.buttonProductos.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.buttonProductos.Location = new System.Drawing.Point(93, 3);
+            this.buttonProductos.Name = "buttonProductos";
+            this.buttonProductos.Size = new System.Drawing.Size(84, 105);
+            this.buttonProductos.TabIndex = 1;
+            this.buttonProductos.Text = "PRODUCTOS";
+            this.buttonProductos.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.buttonProductos.UseVisualStyleBackColor = false;
+            // 
+            // buttonPedidos
+            // 
+            this.buttonPedidos.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonPedidos.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.buttonPedidos.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.buttonPedidos.Font = new System.Drawing.Font("Candara", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonPedidos.Image = global::CapaPresentacion.Properties.Resources.pedidos;
+            this.buttonPedidos.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.buttonPedidos.Location = new System.Drawing.Point(3, 3);
+            this.buttonPedidos.Name = "buttonPedidos";
+            this.buttonPedidos.Size = new System.Drawing.Size(84, 105);
+            this.buttonPedidos.TabIndex = 0;
+            this.buttonPedidos.Text = "PEDIDOS";
+            this.buttonPedidos.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.buttonPedidos.UseVisualStyleBackColor = false;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aRCHIVOToolStripMenuItem,
+            this.aYUDAToolStripMenuItem,
+            this.rESTAURACIÓNToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(873, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // aRCHIVOToolStripMenuItem
+            // 
+            this.aRCHIVOToolStripMenuItem.Name = "aRCHIVOToolStripMenuItem";
+            this.aRCHIVOToolStripMenuItem.Size = new System.Drawing.Size(70, 20);
+            this.aRCHIVOToolStripMenuItem.Text = "ARCHIVO";
+            // 
+            // aYUDAToolStripMenuItem
+            // 
+            this.aYUDAToolStripMenuItem.Name = "aYUDAToolStripMenuItem";
+            this.aYUDAToolStripMenuItem.Size = new System.Drawing.Size(58, 20);
+            this.aYUDAToolStripMenuItem.Text = "AYUDA";
+            // 
+            // rESTAURACIÓNToolStripMenuItem
+            // 
+            this.rESTAURACIÓNToolStripMenuItem.Name = "rESTAURACIÓNToolStripMenuItem";
+            this.rESTAURACIÓNToolStripMenuItem.Size = new System.Drawing.Size(105, 20);
+            this.rESTAURACIÓNToolStripMenuItem.Text = "RESTAURACIÓN";
+            // 
+            // panel1
+            // 
+            this.panel1.BackgroundImage = global::CapaPresentacion.Properties.Resources.fondo_logo;
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(633, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(231, 105);
+            this.panel1.TabIndex = 7;
+            // 
+            // FrmInicio
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(873, 405);
+            this.Controls.Add(this.tableLayoutPanelGeneral);
+            this.MainMenuStrip = this.menuStrip1;
+            this.MinimumSize = new System.Drawing.Size(889, 444);
+            this.Name = "FrmInicio";
+            this.Text = ".:. Sistema de Gestión - Distribuidora de Artículos para Librerías Trantanelli S." +
+    "R.L .:. ";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.tableLayoutPanelGeneral.ResumeLayout(false);
+            this.tableLayoutPanelGeneral.PerformLayout();
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanelGeneral;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.Button buttonReportes;
+        private System.Windows.Forms.Button buttonEmpleados;
+        private System.Windows.Forms.Button buttonTransporte;
+        private System.Windows.Forms.Button buttonClientes;
+        private System.Windows.Forms.Button buttonCategorias;
+        private System.Windows.Forms.Button buttonProductos;
+        private System.Windows.Forms.Button buttonPedidos;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem aRCHIVOToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aYUDAToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem rESTAURACIÓNToolStripMenuItem;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
