6327c1f0cb15f3aa61f12042db64a8b1a728b966
Hernan Martin Demczuk <contacto@hernanmartin.me>
2015-06-22 10:52:34 -0300

    Cambios hasta el video 11

---
+++ aplicacion-empresa/CapaNegocio/NProductos.cs
@@ -0,0 +1,18 @@
+﻿using CapaDatos;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaNegocio
+{
+    public class NProductos
+    {
+        public static DataTable Mostrar(int parNumeroPagina, int parRegistrosPorPagina)
+        {
+            return new DProductos().Mostrar(parNumeroPagina, parRegistrosPorPagina);
+        }
+    }
+}
--- aplicacion-empresa/CapaPresentacion/Productos.cs
+++ aplicacion-empresa/CapaPresentacion/Productos.cs
@@ -7,15 +7,26 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using CapaNegocio;
 
 namespace CapaPresentacion
 {
     public partial class Productos : UserControl
     {
+        int registrosPorPagina = 25;
+        int numeroPagina = 2;
+
         public Productos()
         {
             InitializeComponent();
             this.Dock = DockStyle.Fill;
+            Mostrar();
+        }
+
+        public void Mostrar()
+        {
+            this.Dock = DockStyle.Fill;
+            this.dataGridViewProductos.DataSource = NProductos.Mostrar(numeroPagina, registrosPorPagina);
         }
     }
 }
